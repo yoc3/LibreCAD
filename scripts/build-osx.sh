46ce1d1a5fd09e6fdd62c6acb82dd328245a1f70
Dongxu Li <dongxuli2011@gmail.com>
2013-10-28 14:04:26 -0700

    added building scripts for macx

---
+++ scripts/build-osx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# this script builds LibreCAD in OS/X
+# The dependency required to be able to build LibreCAD:
+# qt, boost, muparser
+
+# the LibreCAD source folder 
+
+SCRIPTPATH="$(dirname "$0")"
+
+cd "${SCRIPTPATH}"/..
+# have to clean up any existing binary files to avoid crashes of bug#422
+rm -rf LibreCAD.app
+
+qmake librecad.pro -r macx-g++
+make -j4
+rm -f LibreCAD.dmg
+macdeployqt LibreCAD.app -verbose=2 -dmg
